d49e223e1754e10ed81b76de610255aa58137fc3
nonidelizisi <nonidelizisi@gmail.com>
2025-12-22 14:27:33 +0200

    Update cli_healthcheck_onlyjson.py

--- cli-client/cli_healthcheck_onlyjson.py
+++ cli-client/cli_healthcheck_onlyjson.py
@@ -1,4 +1,3 @@
-import argparse
 import requests
 import json
 
@@ -8,27 +7,7 @@ def healthcheck(ar):
 
     response = requests.get(url)
     print(response.status_code)
+    print(json.dumps(response.json(), ensure_ascii=False, indent=2))
 
-    if response.status_code == 200:
-      if ar.format.lower() == 'json':
-        print(json.dumps(response.json(), ensure_ascii=False, indent=2))
-      return True
-
-    # Σφάλμα (status ≠ 200)
-    else:
-        if ar.format.lower() == 'json':
-          print(json.dumps(response.json(), ensure_ascii=False, indent=2))
-        return False
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--format",
-    choices=["json"],
-    default="json",
-    help="Only json format is provided"
-)
-args = parser.parse_args()
-
-healthcheck(args)
+healthcheck() 
 
